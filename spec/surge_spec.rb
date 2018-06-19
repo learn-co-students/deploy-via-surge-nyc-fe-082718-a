1bc807d4659b99bb99bad2691710ba4e2cc041f2
Steven G. Harms <steven.harms@flatironschool.com>
2018-06-19 10:31:24 -0400

    Add um, an expectation to a test

--- spec/surge_spec.rb
+++ spec/surge_spec.rb
@@ -8,8 +8,8 @@ describe "The file #{SOURCE_FILE}" do
     expect(file_is_present).to be_truthy, "Make sure you created a file called #{SOURCE_FILE} with your URL in it."
   end
 
-  it 'should contain a url to a surge.sh URL' do
+  it 'should contain a url to a surge.sh URL like http://dusty-rose.surge.sh' do
     url = File.open(SOURCE_FILE).readline
-    url.match(/^http.*\.surge\.sh\z/)
+    expect(url.match(/^http.*\.surge\.sh\z/)).to be_truthy
   end
 end
