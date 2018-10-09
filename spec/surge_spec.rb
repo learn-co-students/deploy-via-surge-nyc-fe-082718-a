cfa2bdac62ef20f9e25389f7d96c1aee71b9473a
Steven G. Harms <steven.harms@flatironschool.com>
2018-10-09 18:01:43 -0400

    Per feedback, loosen up the tests for deploy URL

--- spec/surge_spec.rb
+++ spec/surge_spec.rb
@@ -9,7 +9,7 @@ describe "The file #{SOURCE_FILE}" do
   end
 
   it 'should contain a url to a surge.sh URL like http://dusty-rose.surge.sh' do
-    url = File.open(SOURCE_FILE).readline
-    expect(url.match(/^http.*\.surge\.sh\z/)).to be_truthy
+    url = File.open(SOURCE_FILE).readlines.find{ |x| x =~ /^http/}
+    expect(String(url).match(/^http.*\.surge\.sh\Z/)).to be_truthy
   end
 end
